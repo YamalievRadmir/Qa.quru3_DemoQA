5e0cc96e15aa126082264455c6313adc1a597f4d
Radmir <yaradmir2203@gmail.com>
2022-05-29 12:30:56 +0500

    error correction

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,13 +1,13 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -28,7 +28,7 @@ public class TextBoxTests {
         $("[id=firstName]").setValue("Alex");
         $("[id=lastName]").setValue("Larin");
         $("[id=userEmail]").setValue("alexLar@gmail.com");
-        $(".custom-radio:nth-child(1) > .custom-control-label").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("[id=userNumber]").setValue("3748596032");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
@@ -36,22 +36,21 @@ public class TextBoxTests {
         $(".react-datepicker__year-select").click();
         $(".react-datepicker__year-select").selectOption("1990");
         $(".react-datepicker__day--010:nth-child(3)").click();
-        $(".subjects-auto-complete__value-container").click();
         $("#subjectsInput").setValue("Physics").pressEnter();
-        $(".custom-checkbox:nth-child(2) > .custom-control-label").click();
-        $("#uploadPicture").uploadFile (new File("src/test/java/pictures/Screenshot_1.png"));
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFile (new File("src/test/java/resources/Screenshot_2.png"));
         $("#currentAddress-wrapper").click();
         $("#currentAddress").val("Lenina 5");
         $("#state").scrollTo().click();
-        $("#stateCity-wrapper").$(Selectors.byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#city").click();
-        $("#stateCity-wrapper").$(Selectors.byText("Delhi")).click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
 
         $(".modal-content").shouldHave(text("Thanks for submitting the form"),
                 text("alexLar@gmail.com"),
                 text("alexLar@gmail.com"), text("Male"), text("3748596032"), text("Lenina 5"),
                 text("10 April,1990"), text("Physics"), text("Reading"),
-                text("Screenshot_1.png"), text("Lenina 5"), text("NCR Delhi"));
+                text("Screenshot_2.png"), text("Lenina 5"), text("NCR Delhi"));
     }
 }
